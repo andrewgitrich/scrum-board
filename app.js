d5b99bd9cbac8b0f63bf86f6d3c29111178afd17
Andrew Rich <andrew_rich@hotmail.com>
2016-07-16 14:38:06 -0400

    clean up your code young man

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
-
-
-
 	$('button').click(function(){
 		$('.col-1').append("<textarea></textarea>");
 	// cancel allows the textarea to be dragged freely
-	$('textarea').draggable({
+		$('textarea').draggable({
 		cancel: ''
-	});
-
+		});
 	});
 
 
